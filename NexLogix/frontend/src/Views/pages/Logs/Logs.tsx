fadfc9578f5b5295e72528202911a803e74b8d6b
Brandon Senior Dev <gonzdeiv123@gmail.com>
2026-01-17 16:28:13 -0500

    feature: adding a composer docker instance for indepented database, as well as adding a temporary Log viewer

---
+++ NexLogix/frontend/src/Views/pages/Logs/Logs.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const Logs: FC = () => {
+  return (
+    <div className="container py-4">
+      <h1>Logs</h1>
+      <p>No hay logs disponibles.</p>
+    </div>
+  );
+};
+
+export default Logs;
